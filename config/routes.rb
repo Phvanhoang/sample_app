674f6eb80d0b0643c72be01f15749b93e726b5f8
phvanhoang <hoangpv6681@gmail.com>
2021-03-22 02:17:20 +0700

    Chapter 14: Following users

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ Rails.application.routes.draw do
     get "/help", to: "static_pages#help"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
-    resources :users
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :account_activations, only: :edit
     resources :password_resets, except: [:show, :index, :destroy]
     resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
   end
 end
